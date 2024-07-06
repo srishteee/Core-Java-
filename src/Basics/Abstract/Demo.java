ea8e5967f703af0317fbbc4fe20c22759eae6815
srishteee <srishtinagarkoti.25@gmail.com>
2024-07-06 23:28:00 +0530

    DETAILING ABOUT ABSTRACT CLASS AND METHODS

--- src/Basics/Abstract/Demo.java
+++ src/Basics/Abstract/Demo.java
@@ -1,29 +1,44 @@
 package Basics.Abstract;
-
-
-
-abstract class Car{
-
+abstract class Car {
+    // an abstract class can have an abstract methods as well as non abstract methods, but for declaring the abstract methods you need to have a abstract class.
+    // you can have more than one abstract methods
     public abstract void drive();
-    public void playMusic(){
-        System.out.println("playing music");
+    public abstract void fly();
+    public void playMusic() {
+        System.out.println("playing music...");
     }
-
 }
-
-class WagnoR  extends Car{
+// if you dont write the one abstRACT METHOD OR FORGOT TO IMPLEMENT ON THIS CLASS THEN YOU HAVE TO MAKE THIS CLASS AS A ABSTRACT CLASS
+abstract class WagnoR  extends Car {
+    // it is compulsory to write  abstract methods i.e the drive and fly methods because we are extending the Car class
 
     public void  drive ()
     {
-        System.out.println("driving");
-    }
+        System.out.println("driving...");
 
+    }
 }
+
+
+class UpdateWagnoR extends  WagnoR   // CONCRETE CLASS
+{
+    public void  fly (){
+        System.out.println("flying...");
+    }
+}
+
+
+
 public class Demo {
     public static void main(String[] args) {
-// creating objects,
-        Car obj = new Car();
-        obj.drive();
+// you cannot create an object for abstract class , you can use the reference one for creating the objects
+        // NOW YOU CAN USE UPDATEDWAGNOR FOR CREATE AN OBJ   BECAUSE THAT CLASS IS NON ABSTRACT/ CONCRETE CLASS
+        Car obj = new UpdateWagnoR();
+        // with the help of objects you can call the methods
+           obj.drive();
         obj.playMusic();
+        obj.fly();
+        };
+
     }
-}
+
